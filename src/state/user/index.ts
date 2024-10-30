e217053cd0ef19555ec01d3183ea565934d7c5d6
ERuvinga <ruvingaelie@gmail.com>
2024-10-30 22:27:28 +0200

    redefines Api state for app

--- src/state/user/index.ts
+++ src/state/user/index.ts
@@ -1,9 +1,4 @@
-import {
-  AppointmentDatas,
-  CommentDatas,
-  FILTERACCOUNTS,
-  PostWithCommentsDatas,
-} from '@/Constants/Type';
+import { FILTERACCOUNTS, MobileUserDatas, UserRole } from '@/Constants/Type';
 import { atom } from 'recoil';
 
 export const PlatformInfo = atom({
@@ -17,48 +12,42 @@ export const HomeFilterSelected = atom({
   default: FILTERACCOUNTS.ALL,
 });
 
+export const SearchUserTypeSelected = atom({
+  key: 'SearchUserTypeSelected',
+  default: UserRole.ADMIN_PARC,
+});
+
 export const ProfileFilterSelected = atom({
   key: 'ProfileFilterSelected',
   default: 0,
 });
 
-// Select Datas
-export const PostSelected = atom({
-  key: 'PostSelected',
-  default: {} as PostWithCommentsDatas,
+// Client Datas
+export const ClientUsers = atom({
+  key: 'ClientUsers',
+  default: [] as MobileUserDatas[],
 });
 
-export const PostWithoutLikesSelected = atom({
-  key: 'PostWithoutLikesSelected',
-  default: {} as PostWithCommentsDatas,
+// Client Datas
+export const DealersUses = atom({
+  key: 'DealersUses',
+  default: [] as MobileUserDatas[],
 });
 
-export const CommentSelected = atom({
-  key: 'CommentSelected',
-  default: {} as CommentDatas,
+//Filter datas
+export const ClientUsersFiltered = atom({
+  key: 'ClientUsersFiltered',
+  default: [] as MobileUserDatas[],
 });
 
-//Meeting States
-export const MeetingSelected = atom({
-  key: 'MeetingSelected',
-  default: {} as AppointmentDatas,
+// Client Datas
+export const DealersUsesFiltered = atom({
+  key: 'DealersUsesFiltered',
+  default: [] as MobileUserDatas[],
 });
 
-// Loader Home Pages datas
-export const ForumsLoader = atom({
-  key: 'ForumsLoader',
-  default: true,
-});
-
-export const DebatesLoader = atom({
-  key: 'DebatesLoader',
-  default: true,
-});
-export const PollsLoader = atom({
-  key: 'PollsLoader',
-  default: true,
-});
-export const MeetingLoader = atom({
-  key: 'MeetingLoader',
-  default: true,
+// Client Datas
+export const ResultSearchDatas = atom({
+  key: 'ResultSearchDatas',
+  default: [] as MobileUserDatas[],
 });
